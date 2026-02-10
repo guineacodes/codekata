4a61a6bf1b8c2d030b9475d8aa3878c2a4e03610
Yeojin Cho <orlina2001@naver.com>
2026-02-10 10:30:28 +0900

    Create programmers_002.c

---
+++ programmers_002.c
@@ -0,0 +1,19 @@
+정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+
+제한사항
+0 < num1 ≤ 100
+0 < num2 ≤ 100
+
+문제 url 
+https://school.programmers.co.kr/learn/courses/30/lessons/120805
+
+raw code
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    answer = num1 / num2 ;
+    return answer;
+}
